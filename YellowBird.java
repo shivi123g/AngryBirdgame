ec26925202c17cc3790cd3ff2e4489895d2fdada
Pallav Kumar <146383222+pallav23365@users.noreply.github.com>
2024-11-26 16:38:09 +0000

    Update YellowBird.java

--- YellowBird.java
+++ YellowBird.java
@@ -1,11 +1,18 @@
 package io.github.bird.game.Objects;
 
-import io.github.bird.game.Objects.Bird;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class YellowBird extends Bird {
-
-    public YellowBird() {
-        super("YellowBird.svg"); // Pass the texture path for the yellow bird
+    public YellowBird(World world, float x, float y) {
+        super(world, x, y, "Yellowbird.png");
     }
 
+    @Override
+    public void specialAbility() {
+        // Implement the yellow bird's specific ability
+        System.out.println("Yellow Bird special ability activated!");
+        // Example: Speed boost
+        body.applyLinearImpulse(5f, 0, body.getWorldCenter().x, body.getWorldCenter().y, true);
+        // Additional logic for speed boost can be added here
+    }
 }
