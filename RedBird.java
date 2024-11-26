d900b53f15dc9d5fedd245a08534a837454e9485
Pallav Kumar <146383222+pallav23365@users.noreply.github.com>
2024-11-26 16:37:15 +0000

    Update RedBird.java

--- RedBird.java
+++ RedBird.java
@@ -1,11 +1,17 @@
 package io.github.bird.game.Objects;
 
-import io.github.bird.game.Objects.Bird;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class RedBird extends Bird {
-
-    public RedBird() {
-        super("RedBird.png");
+    public RedBird(World world, float x, float y) {
+        super(world, x, y, "Redbird.png"); // Use the appropriate texture file for the red bird
     }
 
+    @Override
+    public void specialAbility() {
+        // Implement the red bird's specific ability
+        System.out.println("Red Bird special ability activated!");
+        // Example: Increase speed or change trajectory
+        body.applyLinearImpulse(5f, 0, body.getWorldCenter().x, body.getWorldCenter().y, true);
+    }
 }
