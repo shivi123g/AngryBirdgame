2a52c1e493753eac73448ee6c85366adb83864f6
Pallav Kumar <146383222+pallav23365@users.noreply.github.com>
2024-11-26 16:36:31 +0000

    Update BlackBird.java

--- BlackBird.java
+++ BlackBird.java
@@ -1,23 +1,3 @@
-//package io.github.bird.game.Objects;
-//
-//import com.badlogic.gdx.math.Vector2;
-//import com.badlogic.gdx.physics.box2d.World;
-//
-//public class BlackBird extends Bird {
-//    public BlackBird(World world, float x, float y) {
-//        super(world, x, y,
-//            "Blackbird.png",
-//            "sounds/black_bird_flap.wav",
-////            "sounds/black_bird_collision.wav");
-//    }
-//
-//    @Override
-//    public void handleCollision() {
-//        super.handleCollision();
-//        // BlackBird explodes upon collision, dealing area damage.
-//        System.out.println("BlackBird collided and caused an explosion!");
-//    }
-//}
 
 
 package io.github.bird.game.Objects;
